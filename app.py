e69c893e56ff835b5c0de38f0a3bac7349f2c89f
Bruno Davis <91693335+brunodavissr@users.noreply.github.com>
2023-06-09 18:42:09 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,62 @@
+import streamlit as st
+from biblioteca import Livro, Biblioteca
+
+def adicionar_livro(biblioteca):
+    st.subheader("Adicionar Livro")
+    id = st.number_input("ID", format='%.0f')
+    titulo = st.text_input("Título")
+    autor = st.text_input("Autor")
+    genero = st.text_input("Gênero")
+    ano = st.number_input("Ano", format='%.0f')
+
+    if st.button("Salvar"):
+        livro = Livro(id, titulo, autor, genero, ano)
+        biblioteca.adicionar_livro(livro)
+        st.success("Livro adicionado com sucesso.")
+
+def visualizar_livros(biblioteca):
+    st.subheader("Livros")
+    livros = biblioteca.visualizar_livros()
+    for livro in livros:
+        livro_obj = Livro(livro[0], livro[1], livro[2], livro[3], livro[4])
+        st.write(f"ID: {livro_obj.id}, Título: {livro_obj.titulo}, Autor: {livro_obj.autor}, Gênero: {livro_obj.genero}, Ano: {livro_obj.ano}")
+
+def atualizar_livro(biblioteca):
+    st.subheader("Atualizar Livro")
+    id = st.number_input("ID do livro a ser atualizado", format='%.0f')
+    titulo = st.text_input("Novo Título")
+    autor = st.text_input("Novo Autor")
+    genero = st.text_input("Novo Gênero")
+    ano = st.number_input("Novo Ano", format='%.0f')
+
+    if st.button("Atualizar"):
+        livro = Livro(id, titulo, autor, genero, ano)
+        biblioteca.atualizar_livro(livro)
+        st.success("Livro atualizado com sucesso.")
+
+def deletar_livro(biblioteca):
+    st.subheader("Deletar Livro")
+    id = st.number_input("ID do livro a ser deletado", format='%.0f')
+
+    if st.button("Deletar"):
+        biblioteca.deletar_livro(id)
+        st.success("Livro deletado com sucesso.")
+
+def main():
+    st.title("Sistema de Gerenciamento de Biblioteca")
+    biblioteca = Biblioteca('biblioteca.db')
+
+    menu = ["Adicionar Livro", "Visualizar Livros", "Atualizar Livro", "Deletar Livro"]
+    escolha = st.sidebar.selectbox("Menu", menu)
+
+    if escolha == "Adicionar Livro":
+        adicionar_livro(biblioteca)
+    elif escolha == "Visualizar Livros":
+        visualizar_livros(biblioteca)
+    elif escolha == "Atualizar Livro":
+        atualizar_livro(biblioteca)
+    elif escolha == "Deletar Livro":
+        deletar_livro(biblioteca)
+
+if __name__ == "__main__":
+    main()
---
+++ biblioteca.py
@@ -0,0 +1,44 @@
+import sqlite3
+
+class Livro:
+    def __init__(self, id, titulo, autor, genero, ano):
+        self.id = id
+        self.titulo = titulo
+        self.autor = autor
+        self.genero = genero
+        self.ano = ano
+
+class Biblioteca:
+    def __init__(self, db_file):
+        self.conexao = sqlite3.connect(db_file)
+        self.cursor = self.conexao.cursor()
+        self.cursor.execute('''
+            CREATE TABLE IF NOT EXISTS livros (
+                id INTEGER,
+                titulo TEXT,
+                autor TEXT,
+                genero TEXT,
+                ano INTEGER
+            )
+        ''')
+
+    def adicionar_livro(self, livro):
+        self.cursor.execute("INSERT INTO livros (id, titulo, autor, genero, ano) VALUES (?,?,?,?,?)",
+                            (livro.id, livro.titulo, livro.autor, livro.genero, livro.ano))
+        self.conexao.commit()
+
+    def visualizar_livros(self):
+        livros = self.cursor.execute("SELECT * FROM livros")
+        return livros
+
+    def atualizar_livro(self, livro):
+        self.cursor.execute("UPDATE livros SET titulo = ?, autor = ?, genero = ?, ano = ? WHERE id = ?",
+                            (livro.titulo, livro.autor, livro.genero, livro.ano, livro.id))
+        self.conexao.commit()
+
+    def deletar_livro(self, id):
+        self.cursor.execute("DELETE FROM livros WHERE id = ?", [id])
+        self.conexao.commit()
+
+    def fechar_conexao(self):
+        self.conexao.close()
