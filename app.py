b9a91b49cf69802fb07c7625a4eed8dbaf161209
Bruno Davis <91693335+brunodavissr@users.noreply.github.com>
2023-06-12 14:27:26 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 import streamlit as st
+import pandas as pd
 from biblioteca import Livro, Biblioteca
 
-#nesta função, estaremos exibindo na tela do usuário, os campos para que ele possa adicionar um livro a biblioteca
+# Nesta função, estaremos exibindo na tela do usuário os campos para que ele possa adicionar um livro à biblioteca
 def adicionar_livro(biblioteca):
     st.subheader("Adicionar Livro")
     id = st.number_input("ID", step=1, min_value=0)
@@ -11,8 +12,10 @@ def adicionar_livro(biblioteca):
     ano = st.number_input("Ano", step=1, min_value=0)
 
     if st.button("Salvar"):
-        if not id or not titulo or not autor or not genero or not ano:
+        if not titulo or not autor or not genero:
             st.error("Preencha todos os campos")
+        elif biblioteca.livro_esta_na_biblioteca(id):
+            st.error("Já há um livro com este ID na biblioteca. Escolha outro ID")
         else:
             livro = Livro(id, titulo, autor, genero, ano)
             biblioteca.adicionar_livro(livro)
@@ -21,9 +24,10 @@ def adicionar_livro(biblioteca):
 def visualizar_livros(biblioteca):
     st.subheader("Livros")
     livros = biblioteca.visualizar_livros()
-    for livro in livros:
-        livro_obj = Livro(livro[0], livro[1], livro[2], livro[3], livro[4])
-        st.write(f"ID: {livro_obj.id}, Título: {livro_obj.titulo}, Autor: {livro_obj.autor}, Gênero: {livro_obj.genero}, Ano: {livro_obj.ano}")
+    livros_df = pd.DataFrame(livros, columns=["ID", "Título", "Autor", "Gênero", "Ano"])
+    livros_df["ID"] = livros_df["ID"].astype(int)  # Converter a coluna ID para inteiros
+    livros_df["Ano"] = livros_df["Ano"].astype(int)  # Converter a coluna Ano para inteiros
+    st.dataframe(livros_df)
 
 def atualizar_livro(biblioteca):
     st.subheader("Atualizar Livro")
@@ -34,17 +38,25 @@ def atualizar_livro(biblioteca):
     ano = st.number_input("Novo Ano", format='%.0f')
 
     if st.button("Atualizar"):
-        livro = Livro(id, titulo, autor, genero, ano)
-        biblioteca.atualizar_livro(livro)
-        st.success("Livro atualizado com sucesso.")
+        if not titulo or not autor or not genero:
+            st.error("Preencha todos os campos")
+        elif not biblioteca.livro_esta_na_biblioteca(id):
+            st.error("Livro não está no acervo")
+        else:
+            livro = Livro(id, titulo, autor, genero, ano)
+            biblioteca.atualizar_livro(livro)
+            st.success("Livro atualizado com sucesso.")
 
 def deletar_livro(biblioteca):
     st.subheader("Deletar Livro")
     id = st.number_input("ID do livro a ser deletado", format='%.0f')
 
     if st.button("Deletar"):
-        biblioteca.deletar_livro(id)
-        st.success("Livro deletado com sucesso.")
+        if not biblioteca.livro_esta_na_biblioteca(id):
+            st.error("Livro não está no acervo")
+        else:
+            biblioteca.deletar_livro(id)
+            st.success("Livro deletado com sucesso.")
 
 def main():
     st.title("Sistema de Gerenciamento de Biblioteca")
